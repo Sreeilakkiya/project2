bb9f64078c48f9798b15a8e2960d4fdb1fb6935e
Sreeilakkiya <152582990+Sreeilakkiya@users.noreply.github.com>
2023-12-02 09:13:18 +0530

    demo website display

---
+++ demo.js
@@ -0,0 +1,18 @@
+function addNumber(v1,v2)
+{
+sum=v1+v2;
+return sum;}
+result=addNumber(5,3);
+console.log(result);
+result=addNumber(5,33);
+console.log(result);
+result=addNumber(5,32);
+console.log(result);
+result=addNumber(5,31);
+console.log(result);
+result=addNumber(5,39);
+console.log(result);
+result=addNumber(5,38);
+console.log(result);
+result=addNumber(5,39);
+console.log(result);
